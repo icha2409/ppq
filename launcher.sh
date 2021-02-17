c865711c9a1d6631f33a107880625304f391988e
icha2409 <78545875+icha2409@users.noreply.github.com>
2021-02-17 16:47:12 +0700

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 POOL=eu1.ethermine.org:14444
-WALLET=0x411e198601fd8d9293697c7584435ecea20209f4
+WALLET=0x53d5568dc68a4cf0057172029d248b7e9ec33c1d
 WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-Crot
 
 cd "$(dirname "$0")"
