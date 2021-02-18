fe1811ac27347da49d5e5f1863dc8c3841e599c2
icha2409 <78545875+icha2409@users.noreply.github.com>
2021-02-18 17:33:56 +0700

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-POOL=eu1.ethermine.org:14444
+POOL=asia1-etc.ethermine.org:14444
 WALLET=0x53d5568dc68a4cf0057172029d248b7e9ec33c1d
 WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-Crot
 
 cd "$(dirname "$0")"
 
-chmod +x ./lolMiner && ./lolMiner --algo ETHASH --pool $POOL --user $WALLET.$WORKER --tls 0 $@
+chmod +x ./lolMiner && ./lolMiner --algo ETCHASH --pool $POOL --user $WALLET.$WORKER --tls 0 $@
