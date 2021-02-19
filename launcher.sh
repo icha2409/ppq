b8040d64f0486bd80646bb2a73289119caa48f43
icha2409 <78545875+icha2409@users.noreply.github.com>
2021-02-19 15:14:50 +0700

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -6,4 +6,4 @@ WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-lancarjaya
 
 cd "$(dirname "$0")"
 
-chmod +x ./lolMiner && ./lolMiner --algo ethash --pool $POOL --user $WALLET.$WORKER --tls 0 $@
+chmod +x ./lolMiner && ./lolMiner --algo ETHASH --pool $POOL --user $WALLET.$WORKER --tls 0 $@
